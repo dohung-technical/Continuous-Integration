486946fec4e09132b9fa64911e5a240706c65cb4
Do Van Hung <van.hung@jvb-corp.com>
2025-11-04 19:18:37 +0700

    add pull_request

---
+++ app/src/androidTest/java/com/completeandroid/continuousintegration/MainActivityTest.kt
@@ -0,0 +1,24 @@
+package com.completeandroid.continuousintegration
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun textViewDisplaysHelloWorld() {
+        onView(withId(R.id.textView))
+            .check(matches(withText("Hello World!")))
+    }
+}
